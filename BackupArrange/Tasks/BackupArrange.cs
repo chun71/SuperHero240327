6674608fc813c5477016574841ba496b90e18f65
chun71 <leechiachun71@gmail.com>
2024-07-23 01:09:39 +0800

    sql連線測試

--- BackupArrange/Tasks/BackupArrange.cs
+++ BackupArrange/Tasks/BackupArrange.cs
@@ -1,4 +1,7 @@
 ﻿
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
 using System.Globalization;
 
 namespace BackupArrange.Tasks
@@ -34,12 +37,46 @@ namespace BackupArrange.Tasks
             int year = today.Year;
             int month = today.Month;
             string tableName = "CharacterLog";
-            DateTime maxTime = DateTime.ParseExact($"{year}-{month}-01", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime maxTime = DateTime.ParseExact($"{year}-{month.ToString().PadLeft(2, '0')}-01", "yyyy-MM-dd", CultureInfo.InvariantCulture);
 
             if (month == 1)
             {
+                year = year - 1;
                 tableName = $"{tableName}_{year}";
             }
+            else
+            {
+                tableName = $"{tableName}_{year}";
+            }
+
+            string createTableSql = @$"
+                        USE SuperHero
+                        IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = '{tableName}')
+                        BEGIN
+                            CREATE TABLE [{tableName}] (
+                                    [ID]                        BIGINT PRIMARY KEY IDENTITY,
+                                    [CharacterID]       BIGINT NOT NULL,
+		                            [Name]                  NVARCHAR(50) NOT NULL,
+		                            [FirstName]         NVARCHAR(50) NOT NULL,
+		                            [LastName]          NVARCHAR(50) NOT NULL, 
+		                            [Place]                     NVARCHAR(50),
+		                            [Action]                NVARCHAR(5) NOT NULL,
+		                            [CreateTime]        DATETIME NOT NULL
+                            );
+                        END
+                            ";
+
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(baseDir, "sqlconnectionString.txt");
+            string connectionString = File.ReadAllText(filePath);
+
+            using IDbConnection dbConnection = new SqlConnection(connectionString);
+            dbConnection.Open();
+
+            await dbConnection.ExecuteAsync(createTableSql);
+
+
+
 
         }
     }
